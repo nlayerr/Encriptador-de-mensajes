0f7f2ae7808fe8a6dc9ef1d3cc2e943a8dfaa51d
nlayerr <nlayerr@gmail.com>
2024-01-22 23:54:13 -0600

    creacion de archivos restantes

--- main.mjs
+++ main.mjs
@@ -1,10 +1,30 @@
 import encriptarTexto from './encriptador.mjs';
 import desencriptarTexto from './desencriptador.mjs';
 
-const userText = 'Hola mundo!! este es un ejemplo de mensaje'
+
+let textUser = "";
+
+const inicializar = () => {
+    document.getElementById('encriptarBtn').addEventListener('click', () => {
+        textUser = document.getElementById('texto').value;
+        const mensajeEncriptado = encriptarTexto(textUser);
+        document.getElementById('resultado').innerText = `Texto encriptado: ${mensajeEncriptado}`;
+    });
+
+    document.getElementById('desencriptarBtn').addEventListener('click', () => {
+        // Obtiene el texto del textarea y lo guarda en la variable
+        textUser = document.getElementById('texto').value;
+        // Desencripta el texto y muestra el resultado
+        const mensajeDesencriptado = desencriptarTexto(textUser);
+        document.getElementById('resultado').innerText = `Texto desencriptado: ${mensajeDesencriptado}`;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', inicializar);
+/*const userText = 'Hola mundo!! este es un ejemplo de mensaje'
 const encriptador = encriptarTexto(userText);
 console.log(`Tu mensaje encriptado es: ${encriptador}`);
 
 const mensajeEncriptado = encriptador;
 const desencriptar = desencriptarTexto(mensajeEncriptado);
-console.log(`Tu mensaje desencriptado es: ${desencriptar}`);
+console.log(`Tu mensaje desencriptado es: ${desencriptar}`);*/
